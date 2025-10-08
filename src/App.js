59d8ab4f587214dcd839e9e91a1af688e2b2d1a3
stephie-spec <compwork254@gmail.com>
2025-10-08 16:47:56 +0300

    adds product card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 import Login from './components/Login';
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -7,12 +8,34 @@ import AboutUs from './components/AboutUs';
 import Card from './components/Card'
 
 function App() {
+
+  const products = [
+    { id: 1, name: "Product 1", price: 19.99, image: "./images/milk.jpeg" },
+    { id: 2, name: "Product 2", price: 29.99, image: "/product2.png" },
+    { id: 3, name: "Product 3", price: 39.99, image: "/product3.png" },
+    { id: 4, name: "Product 4", price: 49.99, image: "/product4.png" },
+  ];
+
+function addToCart(){}
+
   return (
-    <div className="App">
-      <Header />
-      <AboutUs />
-      <Card username="Tom" email="tomford@gmail.com" phone="+123456789"/>
-      <Footer />
+    <div class="container">
+      <h2>Shopping Cart</h2>
+
+      <div class="row">
+        {products.map((product) => (
+          <div key={product.id} class="col-lg-3 col-md-4 col-sm-6 mb-4">
+            <Card 
+              id={product.id}
+              image={product.image}
+              name={product.name}
+              price={product.price}
+              onAddToCart={() => addToCart(product)}
+            />
+          </div>
+        ))}
+      </div>
+
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,19 @@
 function Card(props){
     return (
-        <div className="card">
-            <p>{props.username}</p>
-            <p>{props.email}</p>
-            <p>{props.phone}</p>
+        <div class="card">
+            <img src={props.image} class="card-img-top" alt={props.name} />
+            <div class="card-body">
+                <h5 class="card-title">{props.name}</h5>
+                <p class="card-text text-primary fw-bold">${props.price}</p>
+                <div class="mt-auto">
+                    <button 
+                        class="btn btn-primary w-100" 
+                        onClick={props.onAddToCart}
+                    >
+                        Add to Cart
+                    </button>
+                </div>
+            </div>
         </div>
     );
 }
